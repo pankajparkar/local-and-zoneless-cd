bb89b3822d7a3dd8ca828a095585ec7648896e5f
Pankaj Parkar <parkarpankaj3@gmail.com>
2025-04-11 10:30:25 +0530

    Hello world example

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'lz-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  template: `Hello {{title}}`,
 })
 export class App {
   title = 'local-and-zoneless-cd';
